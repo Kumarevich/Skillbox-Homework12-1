f7f61b0d1e7b580b3f00ebcf1390ee806cd23769
Igor Makarevich <igor.makarevich@novometgroup.com>
2021-06-28 11:36:58 +0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    bool ship_1[1], ship_2[2], ship_3[3], ship_4[4];
+    std::cout << sizeof (ship_1) << std::endl;
+    std::cout << sizeof (ship_2) << std::endl;
+    std::cout << sizeof (ship_3) << std::endl;
+    std::cout << sizeof (ship_4) << std::endl;
+    return 0;
+}
